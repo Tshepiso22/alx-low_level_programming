17d991f2530095318889b8d5745a5d14555ef566
tshepiso22 <lebepemapula@gmail.com>
2023-02-16 12:16:34 -0800

    hry

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Writes some words and return'
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n")
+
+		return (0);
+}
