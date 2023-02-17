b8b9bbd2fff6100f2d8485e8f954641ff3ffef84
tshepiso22 <lebepemapula@gmail.com>
2023-02-17 14:38:31 -0800

    fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,16 +15,16 @@ int main(void)
 	{
 		for (b = a + 1; b < 10; b++)
 		{
-			for (c = b +1; c < 10; c++)	
-			{	
+			for (c = b + 1; c < 10; c++)
+			{
 				putchar(a + '0');
 				putchar(b + '0');
-				putchar(c + '0');	
-				if(a < 7)	
+				putchar(c + '0');
+				if (a < 7)
 				{
 					putchar(',');
 					putchar(' ');
-				}	
+				}
 			}
 		}
 
