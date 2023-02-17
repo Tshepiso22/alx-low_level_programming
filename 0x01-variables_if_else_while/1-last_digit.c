cb31e064f0846b042e9c5df2b9e19e54eeedfac1
tshepiso22 <lebepemapula@gmail.com>
2023-02-17 14:58:46 -0800

    got it

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	else if (n1 < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n1);
 	else
-		printf("Lastbdigit of %d is %d and is greater than 5\n", n, n1);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n1);
 
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,8 +14,9 @@ int main(void)
 	ch = 'a';
 	while (ch <= 'z')
 	{
-		if (ch != 'e' && ch != 'q)
+		if (ch != 'e' && ch != 'q')
 			putchar(ch);
+		ch++;
 	}
 	putchar('\n');
 	return (0);
