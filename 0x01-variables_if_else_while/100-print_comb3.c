c2a3819844fd39b25f3389871222cb7d39779da2
tshepiso22 <lebepemapula@gmail.com>
2023-02-17 14:31:12 -0800

    Another one

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,22 +13,18 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		if (a == 8)
-		{
-			b = 10;
-			a = 10;
-		}
-
 		for (b = a + 1; b < 10; b++)
 		{
 			putchar(a + '0');
 			putchar(b + '0');
-			putchar(',');
-			putchar(' ');
+			if (a < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		}
-	putchar(8 + '0');
-	putchar(9 + '0');
+	}
+
 	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,27 +15,20 @@ int main(void)
 	{
 		for (b = a + 1; b < 10; b++)
 		{
-			if(a == 7 && b == 8)
-			{
-				a = 10;	
-				b = 10;	
-				c = 10;	
-			}
-			
 			for (c = b +1; c < 10; c++)	
 			{	
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(c + '0');	
-				putchar(',');
-				putchar(' ');
+				if(a < 7)	
+				{
+					putchar(',');
+					putchar(' ');
+				}	
 			}
 		}
 
 	}
-	putchar(7 + '0');
-	putchar(8 + '0');
-	putchar(9 + '0');
 	putchar('\n');
 	return (0);
 }
