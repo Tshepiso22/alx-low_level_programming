dec1d79cf316c71a22b02f9d8f20c4002ae6f748
tshepiso22 <lebepemapula@gmail.com>
2023-02-17 14:48:49 -0800

    y2j

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,16 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 9; a++)
+	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
-		putchar(',');
-		putchar(' ');
-	}
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 
-	putchar(a + '0');
+	}
 	putchar('\n');
 	return (0);
 }
