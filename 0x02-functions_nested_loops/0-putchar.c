33026c390012fd7ca4e57289f32ff9e7a4387d71
tshepiso22 <lebepemapula@gmail.com>
2023-02-21 03:36:13 -0800

    Another push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,5 @@
  */
 int putChar(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchat('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
 	return (0);
 }
