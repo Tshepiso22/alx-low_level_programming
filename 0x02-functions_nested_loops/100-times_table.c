267adc4a4e26bbbca0739091c215a0bca1c752b8
tshepiso22 <lebepemapula@gmail.com>
2023-02-22 16:21:19 -0800

    fixes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,12 +12,12 @@ void print_times_table(int n)
 {
 	int a, b;
 	
-	for (a = 0; a =< n; a++)
+	for (a = 0; a <= n; a++)
 	{	
-		for (b = 0; b =< n; b++)
+		for (b = 0; b <= n; b++)
 		{
 			if (a == n && b == n)
-				break();	
+				break;	
 			if ((a * b) < 10)
 			{
 				_putchar((a * b) + '0');
