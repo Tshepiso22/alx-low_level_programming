340c957dff3fa1da6fb00ddd034ae431ea361e82
tshepiso22 <lebepemapula@gmail.com>
2023-02-24 12:53:36 -0800

    push

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * @n: Function receives as argument
+ *
+ * Description: Prints out intergers
+ *
+ * Return: Void
+ */
+void print_number(int n)
+{
+	_putchar(n + '0');
+}
