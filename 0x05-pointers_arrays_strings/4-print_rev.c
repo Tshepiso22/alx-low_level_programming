554e230b72118ab19a1cded1d2e5a626b0473f9f
tshepiso22 <lebepemapula@gmail.com>
2023-02-27 14:14:58 -0800

    Fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 /*
  * print_rev - Entry point
  * @*s: Receives a pointer as argument
- * 
+ *
  * Description: Prints a string in reverse
  *
  * Return: void
@@ -14,8 +14,9 @@ void print_rev(char *s)
 	int n = strlen(s) - 1;
 	int a = 0;
 
-	for (n = 0; n >= a; n--)
+	for ( ; n >= a; n--)
 	{
 		_putchar(s[n]);
 	}
+	_putchar('\n');
 }
