d9202c3417341d3632ff589b9be2429f0fd0363d
tshepiso22 <lebepemapula@gmail.com>
2023-02-28 06:05:12 -0800

    String to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an int
+ * @s: A string operand
+ *
+ * Description: A function that converts a string of numbers to type int
+ *
+ * Return: The converted string or 0 if none found
+ */
+int _atoi(char *s)
+{
+	int num = 0; int a = 0;
+
+	while (s[a] != '\0')
+	{
+		if (s[a] >= 48 && s[a] <= 57)
+		{
+			num = num * 10 + s[a];
+		}
+	}
+	return (num);
+}
