9a666d05ea1e7cc5fb2fae6d700b3dd6a6ec68c1
tshepiso22 <lebepemapula@gmail.com>
2023-03-02 01:23:47 -0800

    test

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything
 	 */
-	*p = *(a + 2) = 98;
+	*p = a[2] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
